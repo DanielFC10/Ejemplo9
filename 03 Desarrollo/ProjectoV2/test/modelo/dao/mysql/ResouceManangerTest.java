2f519afbb076a8276b10b9912428b1e30bb61f12
andeher9505 <andeher17@gmail.com>
2016-02-10 11:15:24 -0500

    Clase Resource Manager y el test

---
+++ 03 Desarrollo/ProjectoV2/test/modelo/dao/mysql/ResouceManangerTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.dao.mysql;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ResouceManangerTest {
+    
+    public ResouceManangerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getConeccion method, of class ResouceMananger.
+     */
+    @Test
+    public void testGetConeccion() throws SQLException{
+        System.out.println("getConeccion");
+            
+        Connection result = ResouceMananger.getConeccion();
+        result.close();
+        
+    }
+    
+}
